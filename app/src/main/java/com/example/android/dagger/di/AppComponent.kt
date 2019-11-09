fdeff057cced4e314294cfe7adc898967dd88c49
nredjap <hinanias@gmail.com>
2019-11-09 17:39:11 +0900

    update ログインセッションレベルのスコープを追加　MainActivityとSettingsActivityをUserComponent管理下に　UserManagerがUserComponentを持ち、その生存期間を管理　UserManagerはRegistration/LoginComponentと並んでAppComponentから直接取得可能に

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,8 +2,8 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.login.LoginComponent
-import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.user.UserManager
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -18,5 +18,5 @@ interface AppComponent {
 
     fun registrationComponent(): RegistrationComponent.Factory
     fun loginComponent(): LoginComponent.Factory
-    fun inject(activity: MainActivity)
+    fun userManager(): UserManager
 }
--- app/src/main/java/com/example/android/dagger/main/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -42,9 +42,9 @@ class MainActivity : AppCompatActivity() {
      * else carry on with MainActivity
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        (application as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
+        val userManager = (application as MyApplication).appComponent.userManager()
         if (!userManager.isUserLoggedIn()) {
             if (!userManager.isUserRegistered()) {
                 startActivity(Intent(this, RegistrationActivity::class.java))
@@ -55,7 +55,7 @@ class MainActivity : AppCompatActivity() {
             }
         } else {
             setContentView(R.layout.activity_main)
-
+            userManager.userComponent?.inject(this)
             setupViews()
         }
     }
